7fa1a0421ffe198129d5ea3b18048f1e10c9e339
Ismail <liamsirikaf8@gmail.com>
2023-03-12 20:03:30 +0100

    settings.fxml et settings.java (done)

--- src/main/java/com/example/morpion2/App.java
+++ src/main/java/com/example/morpion2/App.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(settings.class.getResource("config.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(settings.class.getResource("settings.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 650);
         stage.setTitle("Settings");
         stage.setScene(scene);
