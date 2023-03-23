61ed81712414b0380699e89ebb1303513d342336
Ismail <liamsirikaf8@gmail.com>
2023-03-23 23:40:21 +0100

    progBar Khedama + lire lfichier config.txt
    :)

--- src/main/java/com/example/morpion2/TestController.java
+++ src/main/java/com/example/morpion2/TestController.java
@@ -1,87 +1,40 @@
 package com.example.morpion2;
 
-import com.example.morpion2.*;
+import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.concurrent.Task;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.net.URL;
+import java.util.HashMap;
 
-import static javafx.application.Application.launch;
+import static com.example.morpion2.modeJeuController.fileModel;
+import static com.example.morpion2.modeJeuController.net;
 
-public class TestController {
+public class TestController extends Application {
+    private Thread progressBarThread;
+
+    private Task<Void> task;
+    @FXML
+    private Button start;
 
     @FXML
     private TextField tf;
-    @FXML
-    private Button startButton;
+
     @FXML
     private ProgressBar progbar;
 
-    private Thread progressBarThread;
-    private Task<Void> task;
-
-    private String fileModel;
-    private MultiLayerPerceptron net;
-
-    private Config config;
-
-
-    @FXML
     public void initialize() throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("modeJeu.fxml"));
-        Parent root = loader.load();
-        modeJeuController controller = loader.getController();
-        fileModel = modeJeuController.fileModel;
-        System.out.println(fileModel);
-
-        net = controller.getNet();
 
     }
-
-
-    public void startTest() {
-        startButton.setDisable(true);
-        task =  task1();
-
-
-        progbar.setProgress(0);
-
-        progbar.progressProperty().bind(task.progressProperty());
-        progressBarThread =  new Thread(task);
-        progressBarThread.start();
-
-
-        progbar.progressProperty().bind(task.progressProperty());
-        task.progressProperty().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
-                progbar.progressProperty().bind(task.progressProperty());
-            }
-        });
-        task.messageProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
-                System.out.println(task.getMessage());
-                tf.setText(task.getMessage());
-            }
-        });
-    }
-
-
+    @FXML
     private Task<Void> task1() {
         return new Task<Void>() {
 
@@ -89,14 +42,18 @@ public class TestController {
             protected Void call() throws Exception {
 
 
-                int epochs = 1000000;
-                
+                int epochs = 100000000;
+
                 try {
+                    System.out.println();
+                    System.out.println("START TRAINING ...");
+                    System.out.println();
                     int[] layers = new int[]{ 9, 9, 9};
-
                     double error = 0.0 ;
-
-                    updateMessage("Learning completed!");
+                    for(int i = 0; i < epochs; i++){
+                        updateMessage("Learning completed!");
+                        updateProgress(i, epochs);
+                    }
 
                     // Sauvegarde du model
                     net.save("src/main/resources/resources/models/"+fileModel);
@@ -113,10 +70,28 @@ public class TestController {
         };
     }
 
+    public void startTest() {
+        task =  task1();
 
-    public static void main(String[] args) {
-        launch(args);
+        progbar.setProgress(0);
+
+        progbar.progressProperty().bind(task.progressProperty());
+
+        task.messageProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+                tf.setText(t1);
+            }
+        });
+
+        new Thread(task).start();
     }
 
 
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
 }
